1e18f3a2a76be20113f34b1a78201ea8a8909250
Julia Frances <104040854+jrf343@users.noreply.github.com>
2023-01-24 11:11:40 -0500

    Edit calculator.js to add basic calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+const display  = document.querySelector(".display");
+
 function add (firstNum, secondNum) {
     let result = firstNum + secondNum;
     return result;
@@ -31,5 +33,69 @@ function operate (firstNum, secondNum, operator) {
         result = divide(firstNum, secondNum);
     }
 
-    console.log(result);
+    display.textContent = result;
+}
+
+const numButtons = document.querySelectorAll('.number');
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let digit = button.textContent;
+        display.textContent += digit;
+    })
+})
+
+const opButtons = document.querySelectorAll('.operator');
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let calculation = button.textContent;
+        display.textContent += calculation;
+    })
+})
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener('click', () => {
+    display.textContent = '';
+})
+
+const equalsButton = document.querySelector("#equals-button");
+equalsButton.addEventListener('click', () => {
+    let firstNum = getFirstNum();
+    let secondNum = getSecondNum();
+    let operator = getOperator();
+    operate(firstNum, secondNum, operator);
+})
+
+function getFirstNum () {
+    let firstNum = '';
+    let input = display.textContent;
+    for (let i = 0; i < input.length; i++) {
+        if (input.charAt(i) === "+" || input.charAt(i) === "-" || input.charAt(i) === "*" | input.charAt(i) === "/") {
+            firstNum = input.slice(0, (i));
+        }
+    }
+    return parseInt(firstNum);
+}
+
+function getSecondNum () {
+    let secondNum = '';
+    let input = display.textContent;
+    for (let i = 0; i < input.length; i++) {
+        if (input.charAt(i) === "+" || input.charAt(i) === "-" || input.charAt(i) === "*" | input.charAt(i) === "/") {
+            secondNum = input.slice(i+1)
+        }
+    }
+    return parseInt(secondNum);
+
+}
+
+function getOperator () {
+    let operator = '';
+    let input = display.textContent;
+    for (let i = 0; i < input.length; i++) {
+        if (input.charAt(i) === "+" || input.charAt(i) === "-" || input.charAt(i) === "*" | input.charAt(i) === "/") {
+            operator = input.charAt(i);
+        }
+    }
+    return operator;
+
 }
